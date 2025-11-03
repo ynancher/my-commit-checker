a946aa92c3ac74412aa1123be081801c2bb62829
Yashasvi Nancherla <ynancher@qti.qualcomm.com>
2025-11-03 16:12:01 +0530

    Update check_commits.py

--- check_commits.py
+++ check_commits.py
@@ -48,7 +48,11 @@ def validate_commit_message(commit, sub_char_limit, desc_char_limit, check_blank
 
     signed_off = lines[-1] if n >= 1 else ""
     subject = lines[0] if n >= 1 else ""
-
+    # description will be checked based on blank line check
+    if check_blank_line.lower() == "true":
+        description = lines[2:-1] if n >= 3 else []
+    else:
+        description = lines[1:-1] if n >= 2 else []
     print(lines)
     print("Now subject....")
     print(subject)
@@ -56,12 +60,6 @@ def validate_commit_message(commit, sub_char_limit, desc_char_limit, check_blank
     print(description)
     print("Now signed off....")
     print(signed_off)
-
-    #description will be checked based on 
-    if check_blank_line.lower() == "true":
-        description = lines[2:-1] if n >= 3 else []
-    else:
-        description = lines[1:-1] if n >= 2 else []
 
 
     errors = []
